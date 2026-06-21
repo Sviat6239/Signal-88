f41c089fdeb76b574a1aff8e0531919e0fab0619
Sviat6239 <sviatoslav6239@gmail.com>
2026-06-21 19:59:52 +0200

    simple code spliter

--- main.c
+++ main.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 
 int main(){    
     FILE *fptr;
+    char **lines = NULL;
+    size_t count = 0;
+    char String[500];
 
     fptr = fopen("code.bas", "r");
-
-    char String[100];
 
     if(fptr == NULL){
         printf("Not able to open the file");
     } else {
-            while(fgets(String, 100, fptr)){
-                printf("%s", String);
+            while(fgets(String, sizeof(String), fptr)){
+                String[strcspn(String, "\n")] = '\0';
+
+                char **tmp = realloc(lines, (count + 1) * sizeof(char*));
+                if (!tmp){
+                    printf("Memory error\n");
+                    break;
+                }
+
+                lines = tmp;
+
+                lines[count] = malloc(strlen(String) + 1);
+                strcpy(lines[count], String);
+
+                count++;
             }
+
+            printf("Our code lines:\n");
+            for (size_t i = 0; i < count; i++){
+                printf("%zu: %s\n", i, lines[i]);
+                free(lines[i]);
+            }
+            free(lines);
     }
 
     fclose(fptr);
+
+    return 0;
 }
