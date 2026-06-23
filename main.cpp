55c100fff61b637a7f8dfa83c5069e4a88e7f9b8
Sviat6239 <sviatoslav6239@gmail.com>
2026-06-23 21:02:05 +0200

    simple text spliter

--- main.cpp
+++ main.cpp
@@ -6,9 +6,18 @@
 
 using namespace std;
 
+string split(const string& str){
+    const string whitespace = " \t\n\r\f\v";
+    size_t first = str.find_first_not_of(whitespace);
+    if (first == string::npos) return "";
+    size_t last = str.find_first_not_of(whitespace);
+    return str.substr(first, (last - first + 1));
+}
+
 int main() {
     string code_line;
     vector<string> lines;
+    vector<string> line;
 
     ifstream code("code.bas");
 
@@ -18,8 +27,20 @@ int main() {
 
     code.close();
 
+    cout << "Our code divided into single line: " << endl;
     for (const string& s : lines){
         cout << "Stored: " << s << endl;
+    }
+
+    
+    for (const string& s : lines){
+        string splited = split(s);
+        line.push_back(splited);
+    }
+
+    cout << "Our striped lines:" << endl;
+    for (const string& s : line){
+        cout << "Splited: " << s << endl;
     }
 
     return 0;
