562adee672c0562ff515b92612d8f84e8c2e0d28
Sviat6239 <sviatoslav6239@gmail.com>
2026-06-23 18:13:24 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    string code_line;
+
+    ifstream code("code.bas");
+
+    while (getline (code, code_line)){
+        cout << code_line << endl;
+    }
+
+    code.close();
+
     return 0;
 }
