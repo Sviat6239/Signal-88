322dbf097cb60af49fb995d5942cec7a7c21365a
Sviat6239 <sviatoslav6239@gmail.com>
2026-06-23 19:48:20 +0200

    added text divider

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
 int main() {
-
     string code_line;
+    vector<string> lines;
 
     ifstream code("code.bas");
 
     while (getline (code, code_line)){
-        cout << code_line << endl;
+        lines.push_back(code_line);
     }
 
     code.close();
+
+    for (const string& s : lines){
+        cout << "Stored: " << s << endl;
+    }
 
     return 0;
 }
