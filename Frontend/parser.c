3362ed04c20b171873583cec2292e306ae7fba3f
Sviat6239 <sviatoslav6239@gmail.com>
2026-08-12 11:43:48 +0200

    fix: update label syntax in example script for consistency

--- Frontend/parser.c
+++ Frontend/parser.c
@@ -12,24 +12,11 @@ Symbol* symbol_table = NULL;
 int symbol_count = 0;
 int symbol_capacity = 0;
 
-typedef struct {
-    char name[64];
-    int target_instruction_index; 
-} Label;
-
-Label* label_table = NULL;
-int label_count = 0;
-int label_capacity = 0;
-
 void init_tables(int initial_capacity) {
     symbol_capacity = initial_capacity;
     symbol_table = (Symbol*)malloc(sizeof(Symbol) * symbol_capacity);
-    
-    label_capacity = initial_capacity;
-    label_table = (Label*)malloc(sizeof(Label) * label_capacity);
-
-    if (symbol_table == NULL || label_table == NULL) {
-        printf("Fatal error: Memory allocation failed during initialization!\n");
+    if (symbol_table == NULL) {
+        printf("Fatal error: Memory allocation failed!\n");
         exit(1);
     }
 }
@@ -45,9 +32,8 @@ void add_symbol(const char* name, const char* data_type) {
     if (symbol_count >= symbol_capacity) {
         symbol_capacity = (symbol_capacity == 0) ? 8 : symbol_capacity * 2;
         Symbol* new_table = (Symbol*)realloc(symbol_table, sizeof(Symbol) * symbol_capacity);
-        
         if (new_table == NULL) {
-            printf("Fatal error: Memory re-allocation failed for symbol table!\n");
+            printf("Fatal error: Reallocation failed for symbol table!\n");
             exit(1);
         }
         symbol_table = new_table;
@@ -68,47 +54,10 @@ const char* lookup_symbol(const char* name) {
     return NULL;
 }
 
-void add_label(const char* name, int position) {
-    for (int i = 0; i < label_count; i++) {
-        if (strcmp(label_table[i].name, name) == 0) {
-            printf("Error: Duplicate label '%s'\n", name);
-            exit(1);
-        }
-    }
-
-    if (label_count >= label_capacity) {
-        label_capacity = (label_capacity == 0) ? 8 : label_capacity * 2;
-        Label* new_table = (Label*)realloc(label_table, sizeof(Label) * label_capacity);
-        
-        if (new_table == NULL) {
-            printf("Fatal error: Memory re-allocation failed for label table!\n");
-            exit(1);
-        }
-        label_table = new_table;
-    }
-
-    strncpy(label_table[label_count].name, name, 63);
-    label_table[label_count].name[63] = '\0';
-    label_table[label_count].target_instruction_index = position;
-    label_count++;
-}
-
-int lookup_label(const char* name) {
-    for (int i = 0; i < label_count; i++) {
-        if (strcmp(label_table[i].name, name) == 0) {
-            return label_table[i].target_instruction_index;
-        }
-    }
-    return -1;
-}
-
 void free_tables() {
     free(symbol_table);
-    free(label_table);
     symbol_table = NULL;
-    label_table = NULL;
     symbol_count = symbol_capacity = 0;
-    label_count = label_capacity = 0;
 }
 
 ASTNode* create_node(ASTNodeType type, int value, const char* name, const char* literal_value, const char* data_type, int mutability, ASTNode* left, ASTNode* right) {
@@ -138,149 +87,290 @@ ASTNode* create_node(ASTNodeType type, int value, const char* name, const char* 
     return node;
 }
 
-ASTNode* parse_statement(TokenList* tokens, int* pos) {
-    Token current = tokens->tokens[*pos];
-
-    if (current.type == TOKEN_LET){
-        (*pos)++;
-
-        int mutability = 0;
-        if (tokens->tokens[*pos].type == TOKEN_MUT || tokens->tokens[*pos].type == TOKEN_IMM){
-            mutability = (tokens->tokens[*pos].type == TOKEN_MUT) ? 1 : 0;
-            (*pos)++;
-        }
-
-        if (tokens->tokens[*pos].type != TOKEN_COLON){
-            printf("Expected colon at pos=%d\n", *pos);
-            exit(1);
-        }
-        (*pos)++;
-
-        const char* d_type = "i32";
-        TokenType t_type = tokens->tokens[*pos].type;
-
-        if(t_type >= TOKEN_I64 && t_type <= TOKEN_CHAR){
-            d_type = tokens->tokens[*pos].data_type;
-            (*pos)++;
-        }
-
-        if (tokens->tokens[*pos].type != TOKEN_COLON){
-            printf("Expected colon at pos=%d\n", *pos);
-            exit(1);
-        }
-        (*pos)++;
-
-        if (tokens->tokens[*pos].type != TOKEN_IDENTIFIER){
-            printf("Syntax error: expected identifier at pos=%d\n", *pos);
-            exit(1);
-        }
-        Token var = tokens->tokens[*pos];
-        (*pos)++;
-
-        if (tokens->tokens[*pos].type != TOKEN_EQUAL){
-            printf("Syntax error: expected '=' at pos=%d\n", *pos);
-            exit(1);
-        }
-        (*pos)++;
-
-        add_symbol(var.name, d_type);
-
-        ASTNode* expr = parse_expression(tokens, pos);
-
-        if (tokens->tokens[*pos].type != TOKEN_SEMICOLON){
-            printf("Syntax error: expected ';' at pos=%d\n", *pos);
-            exit(1);
-        }
-        (*pos)++;
-
-        return create_node(AST_ASSIGN, 0, var.name, NULL, d_type, mutability, expr, NULL);
+void expect_token(TokenList* tokens, int* pos, TokenType expected, const char* err_msg) {
+    if (tokens->tokens[*pos].type != expected) {
+        printf("Syntax error: %s at pos=%d\n", err_msg, *pos);
+        exit(1);
     }
-
-    else if (current.type == TOKEN_PRINT) {
-        (*pos)++;
-
-        if (tokens->tokens[*pos].type != TOKEN_LPAREN) {
-            printf("Syntax error: expected '(' at pos=%d\n", *pos);
-            exit(1);
-        }
-        (*pos)++;
-
-        ASTNode *expr = parse_expression(tokens, pos); 
-
-        if (tokens->tokens[*pos].type != TOKEN_RPAREN) {
-            printf("Syntax error: expected ')' at pos=%d\n", *pos);
-            exit(1);
-        }
-        (*pos)++;
-
-        return create_node(AST_PRINT, 0, NULL, NULL, NULL, 0, expr, NULL);
-    }
-
-    else if (current.type == TOKEN_PRTLN){
-        (*pos)++;
-
-        if (tokens->tokens[*pos].type != TOKEN_SEMICOLON){
-            printf("Syntax error: expected ';' at pos=%d\n", *pos);
-            exit(1);
-        }
-        (*pos)++;
-
-        return create_node(AST_PRTLN, 0, NULL, NULL, NULL, 0, NULL, NULL);
-    }
-
-    else if (current.type == TOKEN_LABEL) {
-        Token label_token = tokens->tokens[*pos];
-        (*pos)++;
-
-        if (tokens->tokens[*pos].type != TOKEN_COLON) {
-            printf("Syntax error: expected ':' after label at pos=%d\n", *pos);
-            exit(1);
-        }
-        (*pos)++;
-
-        add_label(label_token.name, *pos);
-
-        if (tokens->tokens[*pos].type != TOKEN_SEMICOLON){
-            printf("Syntax error: expected ';' after label name at pos=%d\n", *pos);
-            exit(1);
-        }
-        (*pos)++;
-
-        return create_node(AST_LABEL, 0, label_token.name, NULL, NULL, 0, NULL, NULL);
-    }
-
-    else if (current.type == TOKEN_JMP){
-        (*pos)++;
-
-        if (tokens->tokens[*pos].type != TOKEN_LPAREN){
-            printf("Syntax error: expected '(' at pos=%d\n", *pos);
-            exit(1);
-        }
-        (*pos)++;
-
-        ASTNode *expr = parse_expression(tokens, pos);
-
-        if (tokens->tokens[*pos].type != TOKEN_RPAREN){
-            printf("Syntax error: expected ')' at pos = %d\n", *pos);
-            exit(1);
-        }
-        (*pos)++;
-
-        if (tokens->tokens[*pos].type != TOKEN_SEMICOLON){
-            printf("Syntax error: expected ';' at pos = %d\n", *pos);
-            exit(1);
-        }
-        (*pos)++;
-
-        return create_node(AST_JMP, 0, NULL, NULL, NULL, 0, expr, NULL);
-    }
+    (*pos)++;
 }
 
 ASTNode* parse_expression(TokenList* tokens, int* pos) {
     Token current = tokens->tokens[*pos];
     ASTNode* left = NULL;
+
+    if (current.type == TOKEN_NUMBER) {
+        left = create_node(AST_NUMBER, current.value, NULL, NULL, "i32", 0, NULL, NULL);
+        (*pos)++;
+    } 
+    else if (current.type == TOKEN_STR) {
+        left = create_node(AST_STRING, 0, NULL, current.name, "str", 0, NULL, NULL);
+        (*pos)++;
+    }
+    else if (current.type == TOKEN_CHAR) {
+        left = create_node(AST_CHARACTER, current.value, NULL, NULL, "char", 0, NULL, NULL);
+        (*pos)++;
+    }
+    else if (current.type == TOKEN_LET) {
+        left = parse_statement(tokens, pos);
+    }
+    else if (current.type == TOKEN_IDENTIFIER) {
+        Token var = current;
+        (*pos)++;
+
+        if (tokens->tokens[*pos].type == TOKEN_LPAREN) {
+            (*pos)++;
+            ASTNode* args_head = NULL;
+            ASTNode* args_tail = NULL;
+
+            while (tokens->tokens[*pos].type != TOKEN_RPAREN && tokens->tokens[*pos].type != TOKEN_EOF) {
+                ASTNode* arg = parse_expression(tokens, pos);
+                
+                if (args_head == NULL) {
+                    args_head = arg;
+                    args_tail = arg;
+                } else {
+                    args_tail->next = arg;
+                    args_tail = arg;
+                }
+
+                if (tokens->tokens[*pos].type == TOKEN_COMMA) {
+                    (*pos)++;
+                }
+            }
+            expect_token(tokens, pos, TOKEN_RPAREN, "expected ')' after arguments");
+            left = create_node(AST_CALL, 0, var.name, NULL, "void", 0, args_head, NULL);
+        } else {
+            left = create_node(AST_VAR, 0, var.name, NULL, lookup_symbol(var.name), 0, NULL, NULL);
+        }
+    }
+    else if (current.type == TOKEN_LPAREN) {
+        (*pos)++;
+        left = parse_expression(tokens, pos);
+        expect_token(tokens, pos, TOKEN_RPAREN, "expected ')'");
+    }
+    else {
+        printf("Syntax error: unexpected token at pos=%d, type=%d\n", *pos, current.type);
+        exit(1);
+    }
+
+    current = tokens->tokens[*pos];
+    while (current.type == TOKEN_PLUS || current.type == TOKEN_MINUS ||
+           current.type == TOKEN_STAR || current.type == TOKEN_DIV ||
+           current.type == TOKEN_EQUAL_EQUAL || current.type == TOKEN_PLUS_PLUS) {
+
+        if (current.type == TOKEN_PLUS_PLUS) { 
+            (*pos)++;
+            left = create_node(AST_BINARY_OP, '+', NULL, NULL, "i32", 0, left, create_node(AST_NUMBER, 1, NULL, NULL, "i32", 0, NULL, NULL));
+        } else {
+            char op = 0;
+            switch (current.type) {
+                case TOKEN_PLUS: op = '+'; break;
+                case TOKEN_MINUS: op = '-'; break;
+                case TOKEN_STAR: op = '*'; break;
+                case TOKEN_DIV: op = '/'; break;
+                case TOKEN_EQUAL_EQUAL: op = 'E'; break;
+                default: op = 0;   break;
+            }
+            (*pos)++;
+            ASTNode* right = parse_expression(tokens, pos);
+            left = create_node(AST_BINARY_OP, op, NULL, NULL, "i32", 0, left, right);
+        }
+        current = tokens->tokens[*pos];
+    }
+
+    return left;
+}
+
+ASTNode* parse_statement(TokenList* tokens, int* pos) {
+    Token current = tokens->tokens[*pos];
+
+    if (current.type == TOKEN_LET) {
+        (*pos)++;
+
+        int mutability = 0;
+        if (tokens->tokens[*pos].type == TOKEN_MUT || tokens->tokens[*pos].type == TOKEN_IMM) {
+            mutability = (tokens->tokens[*pos].type == TOKEN_MUT) ? 1 : 0;
+            (*pos)++;
+        }
+
+        expect_token(tokens, pos, TOKEN_COLON, "expected ':' after mutability modifier");
+
+        char full_type[64] = "";
+        strcat(full_type, tokens->tokens[*pos].data_type);
+        (*pos)++;
+
+        if (tokens->tokens[*pos].type == TOKEN_LBRACKET) {
+            strcat(full_type, "[");
+            (*pos)++;
+            strcat(full_type, tokens->tokens[*pos].name);
+            (*pos)++;
+            expect_token(tokens, pos, TOKEN_RBRACKET, "expected ']' in array type definition");
+            strcat(full_type, "]");
+        }
+
+        expect_token(tokens, pos, TOKEN_COLON, "expected ':' after type definition");
+
+        Token var = tokens->tokens[*pos];
+        expect_token(tokens, pos, TOKEN_IDENTIFIER, "expected identifier name");
+
+        add_symbol(var.name, full_type);
+
+        ASTNode* expr = NULL;
+        if (tokens->tokens[*pos].type == TOKEN_EQUAL) {
+            (*pos)++;
+            expr = parse_expression(tokens, pos);
+        }
+
+        if (tokens->tokens[*pos].type == TOKEN_SEMICOLON) {
+            (*pos)++;
+        }
+
+        return create_node(AST_ASSIGN, 0, var.name, NULL, full_type, mutability, expr, NULL);
+    }
+
+    else if (current.type == TOKEN_PRINT) {
+        (*pos)++;
+        expect_token(tokens, pos, TOKEN_LPAREN, "expected '(' after print");
+        
+        ASTNode* args_head = NULL;
+        ASTNode* args_tail = NULL;
+
+        while (tokens->tokens[*pos].type != TOKEN_RPAREN && tokens->tokens[*pos].type != TOKEN_EOF) {
+            ASTNode* arg = parse_expression(tokens, pos);
+            if (args_head == NULL) {
+                args_head = arg;
+                args_tail = arg;
+            } else {
+                args_tail->next = arg;
+                args_tail = arg;
+            }
+            if (tokens->tokens[*pos].type == TOKEN_COMMA) (*pos)++;
+        }
+        
+        expect_token(tokens, pos, TOKEN_RPAREN, "expected ')' after print arguments");
+        expect_token(tokens, pos, TOKEN_SEMICOLON, "expected ';' after print");
+
+        return create_node(AST_PRINT, 0, NULL, NULL, NULL, 0, args_head, NULL);
+    }
+
+    else if (current.type == TOKEN_PRTLN) {
+        (*pos)++;
+        expect_token(tokens, pos, TOKEN_SEMICOLON, "expected ';' after prtln");
+        return create_node(AST_PRTLN, 0, NULL, NULL, NULL, 0, NULL, NULL);
+    }
+
+    else if (current.type == TOKEN_LABEL) {
+        (*pos)++;
+        expect_token(tokens, pos, TOKEN_COLON, "expected ':' after label keyword");
+        
+        Token label_var = tokens->tokens[*pos];
+        expect_token(tokens, pos, TOKEN_IDENTIFIER, "expected label name");
+        expect_token(tokens, pos, TOKEN_SEMICOLON, "expected ';' after label name");
+
+        return create_node(AST_LABEL, 0, label_var.name, NULL, NULL, 0, NULL, NULL);
+    }
+
+    else if (current.type == TOKEN_JMP) {
+        (*pos)++;
+        expect_token(tokens, pos, TOKEN_LPAREN, "expected '(' after jmp");
+        
+        Token target = tokens->tokens[*pos];
+        expect_token(tokens, pos, TOKEN_IDENTIFIER, "expected label identifier inside jmp(...)");
+        
+        expect_token(tokens, pos, TOKEN_RPAREN, "expected ')' after label target");
+        expect_token(tokens, pos, TOKEN_SEMICOLON, "expected ';' after jmp statement");
+
+        return create_node(AST_JMP, 0, target.name, NULL, NULL, 0, NULL, NULL);
+    }
+
+    else if (current.type == TOKEN_IF) {
+        (*pos)++;
+        expect_token(tokens, pos, TOKEN_LPAREN, "expected '(' after if");
+        ASTNode* cond = parse_expression(tokens, pos);
+        expect_token(tokens, pos, TOKEN_RPAREN, "expected ')' after condition");
+
+        expect_token(tokens, pos, TOKEN_LBRACE, "expected '{' before if body");
+
+        ASTNode* then_body = NULL;
+        ASTNode* body_tail = NULL;
+
+        while (tokens->tokens[*pos].type != TOKEN_RBRACE && tokens->tokens[*pos].type != TOKEN_EOF) {
+            ASTNode* stmt = parse_statement(tokens, pos);
+            if (then_body == NULL) {
+                then_body = stmt;
+                body_tail = stmt;
+            } else {
+                body_tail->next = stmt;
+                body_tail = stmt;
+            }
+        }
+        expect_token(tokens, pos, TOKEN_RBRACE, "expected '}' after if body");
+
+        ASTNode* else_body = NULL;
+        if (tokens->tokens[*pos].type == TOKEN_ELSE) {
+            (*pos)++;
+            if (tokens->tokens[*pos].type == TOKEN_IF) {
+                else_body = parse_statement(tokens, pos);
+            } else {
+                expect_token(tokens, pos, TOKEN_LBRACE, "expected '{' before else body");
+                
+                ASTNode* else_tail = NULL;
+                while (tokens->tokens[*pos].type != TOKEN_RBRACE && tokens->tokens[*pos].type != TOKEN_EOF) {
+                    ASTNode* stmt = parse_statement(tokens, pos);
+                    if (else_body == NULL) {
+                        else_body = stmt;
+                        else_tail = stmt;
+                    } else {
+                        else_tail->next = stmt;
+                        else_tail = stmt;
+                    }
+                }
+                expect_token(tokens, pos, TOKEN_RBRACE, "expected '}' after else body");
+            }
+        }
+
+        ASTNode* branch_node = create_node(AST_ELSE, 0, NULL, NULL, NULL, 0, then_body, else_body);
+        return create_node(AST_IF, 0, NULL, NULL, NULL, 0, cond, branch_node);
+    }
+
+    else {
+        ASTNode* expr = parse_expression(tokens, pos);
+        if (tokens->tokens[*pos].type == TOKEN_SEMICOLON) {
+            (*pos)++;
+        }
+        return expr;
+    }
+}
+
+ASTNode* parse(TokenList* tokens) {
+    int pos = 0;
+    ASTNode* head = NULL;
+    ASTNode* tail = NULL;
+
+    while (tokens->tokens[pos].type != TOKEN_EOF) {
+        ASTNode* stmt = parse_statement(tokens, &pos);
+        
+        if (stmt != NULL) {
+            if (head == NULL) {
+                head = stmt;
+                tail = stmt;
+            } else {
+                tail->next = stmt;
+                tail = stmt;
+            }
+        }
+    }
+
+    return head;
 }
 
 void print_ast(ASTNode* node, int indent) {
     if (node == NULL) return;
+    for (int i = 0; i < indent; i++) printf("  ");
+    printf("ASTNode [Type: %d, Name: '%s', Type: '%s']\n", node->type, node->name, node->data_type ? node->data_type : "none");
+    
+    if (node->left) print_ast(node->left, indent + 1);
+    if (node->right) print_ast(node->right, indent + 1);
+    if (node->next) print_ast(node->next, indent);
 }
