bd3daab2c425ecfd52581aa3fa33d22774f4a4fc
Sviat6239 <sviatoslav6239@gmail.com>
2026-08-17 18:36:28 +0200

    added NumberNode

--- frontend/ast.py
+++ frontend/ast.py
@@ -6,3 +6,12 @@ class ExpressionNode(ABC):
     def __init__(self, line: int = 1, column: int = 1):
         self.line: int = line
         self.column: int = column
+
+class NumberNode(ExpressionNode):
+    def __init__(self, token: Token):
+        supe().__init__(token.lines, yoken.column)
+        self.token = token
+        self.value = token.value
+
+    def __repr__(self):
+        return f"NumberNode(value={self.value}, line={self.line}, col={self.column})"
