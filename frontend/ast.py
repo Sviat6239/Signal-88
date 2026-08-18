8f19a623118096e0652e293acc2a234d549d1c31
Sviat6239 <sviatoslav6239@gmail.com>
2026-08-18 17:38:46 +0200

    added JleNode class with initialization and representation methods

--- frontend/ast.py
+++ frontend/ast.py
@@ -246,6 +246,14 @@ class JgNode(ExpressionNode):
     def __repr__(self):
         return f"JgNode(target={self.target}, line={self.line}, col={self.column})"
 
+class JleNode(ExpressionNode):
+    def __init__(self, target: ExpressionNode):
+        super().__init__(token.line, token.column)
+        self.target = target
+
+    def __repr__(self):
+        return f"JleNode(target={self.target}, line={self.line}, col={self.column})"
+
 class BlockNode(ExpressionNode):
     def __init__(self, statements: List[ExpressionNode], line: int = 1, column: int = 1):
         super().__init__(statements[0].line if statements else line, statements[0].column if statements else column)
