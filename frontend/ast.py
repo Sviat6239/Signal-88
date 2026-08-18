f6399d1ab420ca75e240c0582b5df9d82d265778
Sviat6239 <sviatoslav6239@gmail.com>
2026-08-18 17:36:30 +0200

    added JneNode class with initialization and representation methods

--- frontend/ast.py
+++ frontend/ast.py
@@ -214,6 +214,14 @@ class JmpNode(ExpressionNode):
     def __repr__(self):
         return f"JmpNode(target={self.target}, line={self.line}, col={self.column})"
 
+class JneNode(ExpressionNode):
+    def __init__(self, target: ExpressionNode):
+        super().__init__(token.line, token.column)
+        self.target = target
+
+    def __repr__(self):
+        return f"JneNode(target={self.target}, line={self.line}, col={self.column})"
+
 class BlockNode(ExpressionNode):
     def __init__(self, statements: List[ExpressionNode], line: int = 1, column: int = 1):
         super().__init__(statements[0].line if statements else line, statements[0].column if statements else column)
