90f91cadf924417ca71734f0b7b599929cf13dd7
Sviat6239 <sviatoslav6239@gmail.com>
2026-08-19 08:36:33 +0200

    added JaeNode class with initialization and representation methods

--- frontend/ast.py
+++ frontend/ast.py
@@ -270,6 +270,14 @@ class JaNode(ExpressionNode):
     def __repr__(self):
         return f"JaNode(target={self.target}, line={self.line}, col={self.column})"
 
+class JaeNode(ExpressionNode):
+    def __init__(self, target: ExpressionNode):
+        super().__init__(token.line, token.column)
+        self.target = target
+
+    def __repr__(self):
+        return f"JaeNode(target={self.target}, line={self.line}, col={self.column})"
+
 class MovNode(ExpressionNode):
     def __init__(self, target: ExpressionNode, source: ExpressionNode):
         super().__init__(token.line, token.column)
